203126b874553dcf38bfa530d52019b5ef0ade37
italian <frolovmaxim86@gmail.com>
2024-04-16 22:29:10 +0300

    update lab2/create_dataset.py: del train_test_split

--- lab2/python_scripts/create_dataset.py
+++ lab2/python_scripts/create_dataset.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 from sklearn.datasets import load_wine
-from sklearn.model_selection import train_test_split
 
 # Data loading
 wine = load_wine()
@@ -12,13 +11,8 @@ y = wine.target  # type: ignore
 df = pd.DataFrame(data=X, columns=wine.feature_names)  # type: ignore
 df['target'] = y
 
-# Splitting the DataFrame into training and test samples
-train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
-
 # Create directories for data storage
-os.makedirs('train', exist_ok=True)
-os.makedirs('test', exist_ok=True)
+os.makedirs('data', exist_ok=True)
 
 # Saving training and test samples to CSV files
-train_df.to_csv('train/wine_train.csv', index=False)
-test_df.to_csv('test/wine_test.csv', index=False)
+df.to_csv('data/wine.csv', index=False)
